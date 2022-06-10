1b9e3e123da3b9e78241d892bf8360c355aa1e03
RootZero <rootzero17@gmail.com>
2022-06-11 02:53:10 +0900

    Feat Rank View, Add arrow img

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,29 @@
 import React from "react"
+import styled from "styled-components";
 //import components
 import Header from '../components/Header';
+import RankContainer from "../components/RankContainer";
 
 export const Home = () => {
   return (
-    <>
+    <StHomeWrap>
         <Header/>
-    </>
+        <StTitle>Best Games</StTitle>
+        <RankContainer/>
+    </StHomeWrap>
   )
 }
+
+const StHomeWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StTitle = styled.h2`
+  color: white;
+  font-size: 30px;
+`;
 
 export default Home;
