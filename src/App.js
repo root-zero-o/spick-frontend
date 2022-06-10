acd92dc9db0245c5196169382703310572adc64a
RootZero <rootzero17@gmail.com>
2022-06-10 23:12:59 +0900

    Feat : header view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-     헤헤헤 히히힣
-    </div>
+    <>
+    </>
   );
 }
 
--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -4,6 +4,9 @@ const GlobalStyle = createGlobalStyle `
 * {
     margin: 0;
     padding: 0;
+    
+}
+body {
     background: #1B2838;
     background: -moz-linear-gradient(left, #1B2838 0%, #304864 52%, #1B2838 100%);
     background: -webkit-linear-gradient(left, #1B2838 0%, #304864 52%, #1B2838 100%);
