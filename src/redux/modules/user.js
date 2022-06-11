f81306e5ba31b13917fd96b840f8c5fa583bba2a
Seokwoo <swoo1429@naver.com>
2022-06-11 10:35:26 +0900

    Feat : login, sign page view complete

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,9 +1,48 @@
 import React from 'react'
 
-const user = () => {
-  return (
-    <div>user</div>
-  )
+
+const initialState = {
+  user:{}
+}
+
+const ADD = "user/ADD";
+const LOAD = "user/LOAD";
+
+export function Add(payload,getState){
+  return{ type:ADD,payload}
+}
+export function Load(payload,getState){
+  return {type:LOAD,payload}
+}
+
+
+export const __addUser=(user)=>{
+  return async function(sispatch,getState){
+
+  }
+}
+
+export const __loadUser=(user)=>{
+  return async function(dispatch,getState){
+
+  }
+}
+
+
+
+function user(state=initialState,action){
+  switch(action.type){
+    case ADD:{
+      return{}
+    }
+    case LOAD:{
+      return{}
+    }
+    default:
+      return{...state};
+
+  }
+  
 }
 
 export default user;
